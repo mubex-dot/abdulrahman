70b41c6fa6ef9daeacbbc5839ac7fd32ce41efe5
mubex-dot <63114194+mubex-dot@users.noreply.github.com>
2022-12-23 14:24:48 +0100

    changed some details

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,13 @@
-import Mubarak from "../images/Mubarak.PNG";
+import Abdulrahman from "../images/Abdulrahman.jpeg";
 
 function Home() {
   return (
     <section className="intro" id="home">
       <h1 className="title home-title">
-        Hi, my name is <strong>Mubarak Ibrahim</strong>
+        Hi, my name is <strong>Abdulrahman</strong>
       </h1>
-      <p className="subtitle home-subtitle">Frontend Developer</p>
-      <img src={Mubarak} alt="Mubarak"></img>
+      <p className="subtitle home-subtitle">Momo Agent</p>
+      <img src={Abdulrahman} alt="Mubarak"></img>
     </section>
   );
 }
